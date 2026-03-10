229c225acb0396aee3d8fcbd7996590336f86ffd
paladin <krizthian.loyal@gmail.com>
2026-03-10 15:25:48 +0800

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "ILAGAY_API_KEY_MO",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
